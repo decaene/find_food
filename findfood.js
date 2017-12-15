d1eb622ba64674effe4d7aa8a86fa421b6ae7856
Codigeek Apps <pbarrera@codigeek.com>
2017-12-15 03:34:27 -0600

    first commit

--- findfood.js
+++ findfood.js
@@ -165,4 +165,44 @@ router.post("/autenticacion",function(req,res){
     });
 });
 
+router.post("/get_tipo_comida",function(req,res){
+    var collection      = datb.collection('Tipo_Comida');
+    collection.aggregate([
+        { $orderby : { "descripcion" : -1 } }
+    ]).toArray(function(err, result){  
+        if(err){
+            var res_err      = {};
+            res_err.status   = "error";
+            res_err.error    = err;
+            res_err.message  = err;
+            res.send(res_err);
+        }else{
+            var res_data      = {};
+            res_data.status   = "success";
+            res_data.message  = "Tipos de comida";
+            res_data.data     = result[0];
+            res.send(res_data);
+        }
+    });
+});
+
+router.post("/get_detalles_restaurante",function(req,res){
+    var collection      = datb.collection('Detalles_Restaurante');
+    collection.aggregate([]).toArray(function(err, result){  
+        if(err){
+            var res_err      = {};
+            res_err.status   = "error";
+            res_err.error    = err;
+            res_err.message  = err;
+            res.send(res_err);
+        }else{
+            var res_data      = {};
+            res_data.status   = "success";
+            res_data.message  = "Detalles Restaurante";
+            res_data.data     = result[0];
+            res.send(res_data);
+        }
+    });
+});
+
 app.use('/',router);
