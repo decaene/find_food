7b899aa0e4aa84c50904c7ac3fa5e3d12a9a1e17
Codigeek Apps <pbarrera@codigeek.com>
2017-12-18 01:22:32 -0600

    first commit

--- runfolder.js
+++ runfolder.js
@@ -1,13 +1,41 @@
-var http = require('http');
+var app = express();
+var dir =  process.cwd();
+app.use(express.static(dir)); //current working directory
+app.use(express.static(__dirname)); //module directory
+var server = http.createServer(app);
 
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
+app.get('/files', function(req, res) {
+ var currentDir =  dir;
+ var query = req.query.path || '';
+ if (query) currentDir = path.join(dir, query);
+ console.log("browsing ", currentDir);
+ fs.readdir(currentDir, function (err, files) {
+     if (err) {
+        throw err;
+      }
+      var data = [];
+      files
+      .forEach(function (file) {
+        try {
+                //console.log("processingile);
+                var isDirectory = fs.statSync(path.join(currentDir,file)).isDirectory();
+                if (isDirectory) {
+                  data.push({ Name : file, IsDirectory: true, Path : path.join(query, file)  });
+                } else {
+                  var ext = path.extname(file);
+                  if(program.exclude && _.contains(program.exclude, ext)) {
+                    console.log("excluding file ", file);
+                    return;
+                  }       
+                  data.push({ Name : file, Ext : ext, IsDirectory: false, Path : path.join(query, file) });
+                }
 
-var serve = serveStatic("./");
-
-var server = http.createServer(function(req, res) {
-  var done = finalhandler(req, res);
-  serve(req, res, done);
+        } catch(e) {
+          console.log(e); 
+        }        
+        
+      });
+      data = _.sortBy(data, function(f) { return f.Name });
+      res.json(data);
+  });
 });
-
-server.listen(8000);
