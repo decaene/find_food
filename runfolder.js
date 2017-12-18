5e7ae3a1d2e99b957a91c83c75d3022eeb83ac6d
Codigeek Apps <pbarrera@codigeek.com>
2017-12-17 22:58:30 -0600

    first commit

--- runfolder.js
+++ runfolder.js
@@ -5,6 +5,19 @@ var path = require('path');
 http.createServer(function (request, response) {
     console.log('request starting...');
 
+        // Website you wish to allow to connect
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    response.setHeader('Access-Control-Allow-Credentials', true);
+
     var filePath = '.' + request.url;
     if (filePath == './')
         filePath = './index.html';
