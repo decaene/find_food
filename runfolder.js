1ba7a02b42568d9043c48fc1f1ed28f146378c0d
Codigeek Apps <pbarrera@codigeek.com>
2017-12-18 01:06:35 -0600

    first commit

--- runfolder.js
+++ runfolder.js
@@ -8,9 +8,5 @@ http.createServer(function (req, res) {
 }).listen(8000);
 
 /* server started */  
-util.puts('> hello world running on port 8000');
+console.log('> hello world running on port 8000');
 
-setTimeout(function () {  
-  util.puts('Throwing error now.');
-  throw new Error('User generated fault.');
-}, 5000);
