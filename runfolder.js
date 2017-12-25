39934b58220b1822813a0d435dbc7d4617cd2a96
Codigeek Apps <pbarrera@codigeek.com>
2017-12-25 12:14:13 -0600

    first commit

--- runfolder.js
+++ runfolder.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'))
+app.use(express.static('menus'));
+app.use(express.static('restaurantes'));
 
 var server = app.listen(5000);
