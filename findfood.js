103d207b6c6cfc230ffffe2c689c31dc0b676a66
Codigeek Apps <pbarrera@codigeek.com>
2018-02-11 20:25:46 -0600

    first commit

--- findfood.js
+++ findfood.js
@@ -642,7 +642,7 @@ router.post("/get_restaurantes_publicaciones",function(req,res){
             res.send(res_err);
         }
         else{
-			
+			console.log(result);
 			for(var i = 0; i < result.length; i++){
 				collection.find({ "usuario_id" : ObjectId(req.body.data.usuario_id), "publicacion_id" : ObjectId(result[i].id) })
 				.toArray(function(err, its_liked_by_user){ 
